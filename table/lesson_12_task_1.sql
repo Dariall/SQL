868e4aee412fc2ed7bd22b6feca34ec4f42af911
Dariall <91183648+Dariall@users.noreply.github.com>
2025-07-26 22:33:49 +0300

    Update lesson_12_task_1.sql

--- table/lesson_12_task_1.sql
+++ table/lesson_12_task_1.sql
@@ -1,1 +1,10 @@
-
+IF NOT EXISTS (
+    SELECT * FROM sysobjects WHERE name = N'Студенты' AND xtype = 'U'
+)
+BEGIN
+    CREATE TABLE Студенты (
+        Идентификатор INT PRIMARY KEY,
+        Имя NVARCHAR(100) NOT NULL,
+        Фамилия NVARCHAR(100) NOT NULL
+    );
+END
